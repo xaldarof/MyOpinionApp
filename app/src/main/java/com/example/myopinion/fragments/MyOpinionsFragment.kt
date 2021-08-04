a99dfec043c10eb555ed98871379d90ae967f242
xaldarof <temuruz2003@gmail.com>
2021-08-04 12:28:00 +0500

    First commit

--- app/src/main/java/com/example/myopinion/fragments/MyOpinionsFragment.kt
+++ app/src/main/java/com/example/myopinion/fragments/MyOpinionsFragment.kt
@@ -1,24 +1,16 @@
 package com.example.myopinion.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.lifecycle.ViewModelProvider
-import com.example.myopinion.R
-import com.example.myopinion.adapters.ItemAdapter
-import com.example.myopinion.adapters.MyOpinionsItemAdapter
 import com.example.myopinion.databinding.FragmentMyOpinionsBinding
 import com.example.myopinion.helpers.BundleSender
 import com.example.myopinion.helpers.BundleSenderProvider
 import com.example.myopinion.helpers.MyOpinionAdapterHelper
 import com.example.myopinion.helpers.MyOpinionsAdapterHelperProvider
-import com.example.myopinion.models.Opinion
-import com.example.myopinion.repository.entity.OpinionEntity
 import com.example.myopinion.viewmodel.MyOpinionsFragmentViewModel
 import com.example.myopinion.viewmodel.MyOpinionsViewModelFactory
 import io.realm.Realm
